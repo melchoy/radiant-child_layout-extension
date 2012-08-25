27a728f4cc277cf945756240985f28bc43e5f690
Melissa Choy <mel@otilas.com.au>
2012-08-26 00:29:09 +1000

    cleanup

--- child_layout_extension.rb
+++ child_layout_extension.rb
@@ -14,18 +14,18 @@ class ChildLayoutExtension < Radiant::Extension
     Page.class_eval do
       
       belongs_to :set_layout, :class_name => 'Layout', :foreign_key => 'layout_id'
-   		belongs_to :child_layout, :class_name => 'Layout', :foreign_key => "child_layout_id"
+      belongs_to :child_layout, :class_name => 'Layout', :foreign_key => "child_layout_id"
    		
-   		# Override layout to check for a passed child layout before inheriting the parent set layout
-			def layout
-			  unless set_layout
+      # Override layout to check for a passed child layout before inheriting the parent set layout
+      def layout
+        unless set_layout
           if parent?
             parent.child_layout ? parent.child_layout : parent.layout
           end
         else
           set_layout
         end
-		  end
+      end
 		  
     end    
   end
